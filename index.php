5566f0342f8c284c7f28c76e6ad73c24a13d11e0
Kevin R Costa <kevin_r_costa@estudante.sc.senai.br>
2019-09-30 19:56:07 -0300

    novo usuario formulario
    Formulário de novo usuario

--- index.php
+++ index.php
@@ -105,6 +105,33 @@
         </section>
         <!-- Fim da Seção de Recuperação de Senha -->
 
+        <!-- Início do formulário de cadastro de novos usuários -->
+        <section class="row mt-5">
+            <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaRegistro">
+                <h2 class="text-center mt-2">Registre-se aqui</h2>
+                <form action="#" method="post" class="p-2" id="formRegistro">
+                    <div class="form-group">
+                        <input type="text" name="nomeCompleto" id="nomeCompleto" class="form-control" placeholder="Nome Completo">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="Nome de usuário">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail">
+                    </div>
+
+                    <div class="form-group">
+                        <input type="password" name="senhaUsuario" id="senhaUsuario" class="form-control" placeholder="Senha">
+                    </div>
+
+                </form>
+            </div>
+        </section>
+
+        <!-- Final do formulário de cadastro de novos usuários -->
+
     </main>
 
     <!-- Optional JavaScript -->
