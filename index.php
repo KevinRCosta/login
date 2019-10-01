850285e7287607ff54126c2784314944f945698f
Kevin R Costa <kevin_r_costa@estudante.sc.senai.br>
2019-09-30 21:51:43 -0300

    Mostrar e Ocultar formulários

--- index.php
+++ index.php
@@ -16,7 +16,9 @@
     <title>Sistema de Login</title>
 
     <style>
-        #alerta, #caixaSenha, #caixaRegistro{
+        #alerta,
+        #caixaSenha,
+        #caixaRegistro {
             display: none;
         }
     </style>
@@ -38,7 +40,7 @@
         <section class="row">
             <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
                 <h2 class="text-center mt-2">
-                    Entrar no sitema
+                    Entrar no sistema
                 </h2>
                 <form action="#" method="post" class="p-2" id="formLogin">
                     <div class="form-group">
@@ -170,8 +172,30 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <script src="">
-    // Código jQuery para mostrar e ocultar os formulários
+
+    <script>
+        // Código jQuery para mostrar e ocultar os formulários
+        $(function() {
+            $("#btnEsqueci").click(function() {
+                $("#caixaLogin").hide(); //Ocultar
+                $("#caixaSenha").show(); //Mostrar
+            });
+
+            $("#btnRegistrarNovo").click(function() {
+                $("#caixaLogin").hide(); //Ocultar
+                $("#caixaRegistro").show(); //Mostrar
+            })
+        });
+
+        $("#btnJaRegistrado").click(function() {
+            $("#caixaSenha").hide();
+            $("#caixaLogin").show();
+        })
+
+        $("#btnJaRegistrado2").click(function() {
+            $("#caixaRegistro").hide();
+            $("#caixaLogin").show();
+        })
     </script>
 </body>
 
